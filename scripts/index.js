7325f4cc3a207da0a53834d021571ddd30bd3929
kratus66 <diegoherrera1685@hotmail.com>
2024-04-15 16:31:04 -0500

    TERCER AVANCE DEL PROYECTO M1

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-console.log("estamos ejecutando javascript");
 
 class Activity {
     constructor(id, title, description, url) {
@@ -8,93 +7,80 @@ class Activity {
         this.url = url;
     }
 
-    // Método para crear la tarjeta y agregarla al DOM
+
+ 
+
     createCard() {
-        // Creo el contenedor de la tarjeta
         const newdiv = document.createElement("div");
         newdiv.classList.add("card-container");
 
-        // Creo un nuevo div donde va a ir el título
         const tituloTarjeta = document.createElement("p");
         tituloTarjeta.textContent = this.title;
         newdiv.appendChild(tituloTarjeta);
 
-        // Creo un div donde va a ir la descripción
         const div2 = document.createElement("div");
         div2.textContent = this.description;
+        div2.classList.add("card-description");
         newdiv.appendChild(div2);
 
-        // Creo un enlace
-        const enlace = document.createElement("a");
-        enlace.textContent = this.url;
+        const enlace = document.createElement("img");
+        enlace.src = this.url;
+        enlace.classList.add("card-img");
         newdiv.appendChild(enlace);
 
-        // Agrego el contenedor al body
-        document.body.appendChild(newdiv);
-    }
+        const btnEliminar=document.createElement("i");
+        btnEliminar.classList.add("fa-solid");
+        btnEliminar.classList.add("fa-rectangle-xmark");
+        btnEliminar.classList.add("close-card");
+
+        newdiv.appendChild(btnEliminar);
+
+        btnEliminar.addEventListener("click",function () {
+            newdiv.remove();
+        });
+
+        document.body.appendChild(newdiv);     
+}
 }
 
-// Ejemplo de uso
-/*const actividad = new Activity(1, "Título de la tarjeta", "Descripción de la tarjeta", "http://www.example.com");*/
-/*actividad.createCard();*/
-
-
-class Repository{
-    constructor(){
-        this.activities=[];
+class Repository {
+    constructor() {
+        this.activities = [];
+        this.index = 0; // Move index definition here
     }
 
-
-    // metodos que reciba una actividad y la guarde en el array
-    reciveInf(){
-        //obtener los datos de los inputs
-        
-        const title= document.getElementById("title").value;
-        const description=document.getElementById("description").value;
-        const url=document.getElementById("url-imagen").value;
-        if(title==""|| description==""|| url==""){
+    receiveInf(){
+        const title = document.getElementById("title").value;
+        const description = document.getElementById("description").value;
+        const url = document.getElementById("url-imagen").value;
+        if (title === "" || description === "" || url === "") {
             return alert("Diligencia todos los campos del formulario");
-        }else{
-            this.addActivity(title,description,url);
-            
+        } else {
+            this.addActivity(title, description, url);
         }
-        document.getElementById("title").value="";
-        document.getElementById("description").value="";
-        document.getElementById("url-imagen").value="";
-        
-    };
-
-    addActivity(title,description,url){
-        let index=0;
-        const actividad= new Activity(index,title,description,url);
-        actividad.createCard();
-        /*const activity={
-            index:index,
-            title:title,
-            description:description,
-            url:url
-        }
-        this.activities.push(activity);*/
-        index++;
+        document.getElementById("title").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("url-imagen").value = "";
     }
-    imprimir(){
-        const boton=document.getElementById("btn-submit");
-        boton.addEventListener("click",()=>{
-            this.reciveInf();
+
+    addActivity(title, description, url) {
+        const actividad = new Activity(this.index, title, description, url);
+        actividad.createCard();
+        this.index++;
+    }
+
+    imprimir() {
+        const boton = document.getElementById("btn-submit");
+        boton.addEventListener("click", () => {
+            this.receiveInf();
         });
     }
-    
-    
-    
-    
-    // metodo que permita retornar todas las actividades
-   /*  getAllActivities() {
-        return this.activities;
-    } */
+
+    }
+        
 
 
-    // un metodo que permita filtrar las actividades 
-}
-const repository= new Repository();
+const repository = new Repository();
 repository.imprimir();
+
 
